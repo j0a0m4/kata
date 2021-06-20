ed408fe9b888e7252e74ed626d8a7ac8fa8c28f3
João Marcos Queiroz Lopes <joao.lopes@sensedia.com>
2021-06-20 02:17:42 -0300

    refactor: better solution

--- python/split_strings.py
+++ python/split_strings.py
@@ -1,39 +1,49 @@
+from functools import reduce, partial
+
+
 def compose(*functions):
-    from functools import reduce
     return reduce(lambda f, g: lambda x: f(g(x)), functions, lambda x: x)
 
 
 def solution(s: str) -> list:
-    return compose(splitPair, addUnderscore)(s)
-
-
-def splitPair(s: str) -> list:
-    buf = []
-    for i in range(0, len(s), 2):
-        cut = s[i: i + 2]
-        buf.append(cut)
-    return buf
-
-
-def addUnderscore(s: str) -> str:
-    return (
-        s+"_"
-        if compose(isOdd, len)(s)
-        else s
+    fn = compose(
+        split_pair,
+        partial(append_underscore, s),
+        compose(is_odd, len)
     )
+    return fn(s)
 
 
-def isOdd(i: int) -> bool:
+def split_pair(s: str) -> list:
+    return [extract_pair(s, i) for i in range(0, len(s), 2)]
+
+
+def extract_pair(s: str, i: int) -> str:
+    return s[i: i + 2]
+
+
+def add_underscore(s: str) -> str: return s+"_"
+
+
+def append_underscore(s: str, is_odd: bool) -> str:
+    return add_underscore(s) if is_odd else s
+
+
+def is_odd(i: int) -> bool:
     return i % 2 != 0
 
 
-tests = (
+scenarios = (
     ("", []),
     ("asdfads", ['as', 'df', 'ad', 's_']),
-    ("asdfadsf", ['as', 'df', 'ad', 'sf']),
+    ("asdfadsf", ['as', 'df', 'ad', 's']),
     ("x", ["x_"]),
 )
 
-for inp, exp in tests:
-    actual = solution(inp)
-    assert actual == exp, "fail"
+for example, expected in scenarios:
+    actual = solution(example)
+    warning = f"""FAIL! 
+    Case: solution(\"{example}\")
+    Expected: {expected}
+    Actual: {actual}"""
+    assert actual == expected, warning
